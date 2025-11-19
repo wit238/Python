930ec2257b2256498bc02e899c173edfb66649d1
wit <wewe20489@gmail.com>
2025-11-19 15:38:27 +0700

    Refactor headers and image captions in Home-1.py for improved clarity and consistency

--- Home-1.py
+++ Home-1.py
@@ -5,18 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.header('โปรเจคทำนายชนิดของดอกไอริสด้วย KNN')
-st.image("./img/wit.jpg")
+st.title('โปรเจคทำนายชนิดของดอกไอริสด้วย KNN')
+st.subheader('เว็บแอปพลิเคชันสำหรับทำนายสายพันธุ์ดอกไอริส')
+
+st.sidebar.image("./img/wit.jpg")
+
+st.markdown("---")
+
+st.write("## สายพันธุ์ดอกไอริส (Iris Species)")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Versicolor")
-   st.image("./img/iris1.jpg")
+   st.subheader("Versicolor")
+   st.image("./img/iris1.jpg", caption="Iris Versicolor")
 
 with col2:
-   st.header("Verginiga")
-   st.image("./img/iris2.jpg")
+   st.subheader("Virginica")
+   st.image("./img/iris2.jpg", caption="Iris Virginica")
 
 with col3:
-   st.header("Setosa")
-   st.image("./img/iris3.jpg")
+   st.subheader("Setosa")
+   st.image("./img/iris3.jpg", caption="Iris Setosa")
