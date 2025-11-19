51de5aec31db57da7ecdc18c040055674c855556
wit <wewe20489@gmail.com>
2025-11-19 15:15:14 +0700

    Refactor code structure for improved readability and maintainability

---
+++ Home-1.py
@@ -0,0 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.header('wit')
+st.image("./pic/wit.jpg")
