ba3b8d41b90b759f58cfa90caaaab07d5bcec795
wit <wewe20489@gmail.com>
2025-11-19 15:32:14 +0700

    Fix image path in Home-1.py for correct display

--- Home-1.py
+++ Home-1.py
@@ -3,8 +3,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 st.header('wit')
-st.image("./pic/wit.jpg")
+st.image("./img/wit.jpg")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
