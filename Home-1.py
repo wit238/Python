cda399dbb876260f08fc01fd4173e0acc31c983b
wit <wewe20489@gmail.com>
2025-11-19 15:41:29 +0700

    Add data visualization and statistics display for Iris species in Home-1.py

--- Home-1.py
+++ Home-1.py
@@ -26,3 +26,30 @@ with col2:
 with col3:
    st.subheader("Setosa")
    st.image("./img/iris3.jpg", caption="Iris Setosa")
+
+
+html_7 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+
+dt = pd.read_csv("./data/iris.csv")
+st.write(dt.head(10))
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   #st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
